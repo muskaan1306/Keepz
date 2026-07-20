ed6c06702c8dc004b394f5dd63b418db0ab3ff6c
Muskaan Rastogi <76604711+muskaan1306@users.noreply.github.com>
2026-07-20 13:59:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,54 +5,68 @@ import numpy as np
 
 # Page Configuration
 st.set_page_config(
-    page_title="Sample Dashboard",
+    page_title="Keepz,
     page_icon="📊",
     layout="wide"
 )
 
 # Dashboard Title
-st.title("📊 Sales Dashboard")
-st.markdown("A basic Streamlit dashboard example")
+st.title("📊 Keepz Dashboard")
+st.markdown("A dashboard to view attrition")
 
 # Sidebar
-st.sidebar.header("Filters")
+# st.sidebar.header("Filters")
 
-num_days = st.sidebar.slider(
-    "Select Number of Days",
-    min_value=7,
-    max_value=30,
-    value=14
-)
+# num_days = st.sidebar.slider(
+#     "Select Number of Days",
+#     min_value=7,
+#     max_value=30,
+#     value=14
+# )
 
-# Sample Data
-dates = pd.date_range(end=pd.Timestamp.today(), periods=num_days)
-sales = np.random.randint(100, 1000, size=num_days)
+df=pd.read_csv("employee_attrition_dataset.csv")
 
-df = pd.DataFrame({
-    "Date": dates,
-    "Sales": sales
-})
+# # Sample Data
+# dates = pd.date_range(end=pd.Timestamp.today(), periods=num_days)
+# sales = np.random.randint(100, 1000, size=num_days)
+
+# df = pd.DataFrame({
+#     "Date": dates,
+#     "Sales": sales
+# })
 
 # KPI Section
-col1, col2, col3 = st.columns(3)
+col1, col2, col3, col4, col5 = st.columns(5)
 
-with col1:
-    st.metric("Total Sales", f"${df['Sales'].sum():,.0f}")
+# with col1:
+#     st.metric(f"df2['emp_id']", df"${df['Sales'].sum():,.0f}")
 
 with col2:
-    st.metric("Average Sales", f"${df['Sales'].mean():,.0f}")
+    st.metric(" Attrition Risk", f"{df['attrition_risk_score']}")
 
 with col3:
-    st.metric("Max Sales", f"${df['Sales'].max():,.0f}")
+    st.metric("business impact if exits", f"${df['business_impact_score']}")
+
+
+# with col1:
+#     st.metric("Total Sales", f"${df['Sales'].sum():,.0f}")
+
+# with col2:
+#     st.metric("Average Sales", f"${df['Sales'].mean():,.0f}")
+
+# with col3:
+#     st.metric("Max Sales", f"${df['Sales'].max():,.0f}")
 
 # Chart Section
-st.subheader("Sales Trend")
-st.line_chart(df.set_index("Date"))
+# st.subheader("Sales Trend")
+# st.line_chart(df.set_index("Date"))
 
-# Data Table
-st.subheader("Sales Data")
-st.dataframe(df, use_container_width=True)
+# # Data Table
+# st.subheader("Sales Data")
+# st.dataframe(df, use_container_width=True)
 
 # Footer
+st.markdown("---")
+st.caption("Built with Streamlit")
 st.markdown("---")
 st.caption("Built with Streamlit")
